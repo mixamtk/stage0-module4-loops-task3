aeca4286bf2a578aba0f876a1fce004726eb40bf
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-06 12:12:59 +0300

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        if (t<0) t = t * -1;
+        int sumNumber = 0;
+        String tStr = Integer.toString(t);
+        for ( int i = 0; i < tStr.length(); i++) {
+            sumNumber += Character.getNumericValue((tStr.charAt(i)));
+        }
+        System.out.println(sumNumber);
 
     }
 }
